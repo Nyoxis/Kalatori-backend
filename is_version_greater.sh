de6a78f997f5eb28832b1a85c70517ae670238c1
Nyoxis <omicronnyoxis@gmail.com>
2024-03-07 06:27:45 +0200

    ci: Adjustments for AssetHub, version check and release

--- is_version_greater.sh
+++ is_version_greater.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 IFS=.
 version=($1)
-before_version=($2)
-# starting from minor of version if version shorter before_version
+target_version=($2)
+# starting from minor of version if version shorter target_version
 # fill absent fields in version with zeros
-for ((i=${#version[@]}; i<${#before_version[@]}; i++))
+for ((i=${#version[@]}; i<${#target_version[@]}; i++))
 do
     version[i]=0
 done
 # starting from major of version
 for ((i=0; i<${#version[@]}; i++))
 do
-    if [[ -z ${before_version[i]} ]]
+    if [[ -z ${target_version[i]} ]]
     then
-        # if before_version shorter version then
-        # fill absent fields in before_version with zeros
+        # if target_version shorter version then
+        # fill absent fields in target_version with zeros
         ver2[i]=0
     fi
-    if ((10#${version[i]} > 10#${before_version[i]}))
+    if ((10#${version[i]} > 10#${target_version[i]}))
     then
-        # if version greater than before_version in most major differing field
+        # if version greater than target_version in most major differing field
         exit 0
     fi
-    if ((10#${version[i]} < 10#${before_version[i]}))
+    if ((10#${version[i]} < 10#${target_version[i]}))
     then
-        # if version is not greater in most major differing field
+        # if version less than target_version in most major differing field
+        echo "$1 is less than $2"
         exit 1
     fi
 done
+echo "$1 is equal to $2"
 exit 1
